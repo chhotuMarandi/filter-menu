4d9c1babf13dde7e5503478acaaf7193a63ba789
chhotu marandi <cmarandi29@gmail.com>
2023-04-28 23:42:53 +0530

    add js file

---
+++ script.js
@@ -0,0 +1,37 @@
+const menu = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Chicken Butter Masala",
+    category: "breakfast",
+    price: "$45.99",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2067405/pexels-photo-2067405.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    title: "Chicken Butter Masala",
+    category: "breakfast",
+    price: "$88.99",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/3702403/pexels-photo-3702403.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    title: "Chicken Butter Masala",
+    category: "dinner",
+    price: "$62.99",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/2732666/pexels-photo-2732666.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    title: "Paneer Butter Masala",
+    category: "lunch",
+    price: "$545.99",
+  },
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+ const menuItem = menu.map((item) => {
+  
+ });
+ console.log(menuItem)
+})
